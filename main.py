c9cd9a53277c1d7498f8851b67691edbf5a331e3
Saima Yunus <saimayunus925@gmail.com>
2023-12-26 22:00:34 -0800

    made connection object to connect to our SQLite DB

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-import os, requests # os: getenv() from there gets env variables
+import os, requests, sqlite3 # os: getenv() from there gets env variables
 # requests: we'll use this to fetch data from the API with HTTP requests
+# sqlite3: we'll use this to make and connect to a SQL database for storing/querying the API data
 
 API_KEY = os.getenv('API_KEY') # our API key
 
-def get_content(URL):
-    # reads in API endpoint/URL, returns content there
-    return requests.get(URL) # we made a GET request for this URL, we're returning the HTTP response
+def get_response(URL):
+    # reads in API endpoint/URL, sends a GET request there, returns response status code and response data
+    return (requests.get(URL).status_code, requests.get(URL).content)
 
 API_URL_NUTRIENTS = os.getenv('API_URL_NUTRIENTS') # the API URL for getting recipes by nutrients
 API_URL_INGREDIENTS = os.getenv('API_URL_INGREDIENTS') # the API URL for getting recipes by ingredients
 
-
-
+CONNECTION = sqlite3.connect("recipeDatabase.db") # connection obj to our database
