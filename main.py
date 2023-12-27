1b8ca325d7f54522cf4531d60239385e74c31bd7
Saima Yunus <saimayunus925@gmail.com>
2023-12-26 21:51:06 -0800

    working on API endpoint function

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import os, requests # os: getenv() from there gets env variables
 
 API_KEY = os.getenv('API_KEY') # our API key
 
-def get_content(API_URL):
+def get_content(URL):
     # reads in API endpoint/URL, returns content there
-    return requests.get(API_URL) # we made a GET request for this URL, we're returning the HTTP response
+    return requests.get(URL) # we made a GET request for this URL, we're returning the HTTP response
+
+API_URL_NUTRIENTS = os.getenv('API_URL_NUTRIENTS') # the API URL for getting recipes by nutrients
+API_URL_INGREDIENTS = os.getenv('API_URL_INGREDIENTS') # the API URL for getting recipes by ingredients
+
+
 
