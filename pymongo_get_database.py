ad67ee86c130606eb4ef254559f40fa74eed3f6a
Saima Yunus <saimayunus925@gmail.com>
2023-12-31 14:34:55 -0800

    added a 'get table/collection' function

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -6,3 +6,8 @@ def get_DB():
     CONNECTION_STRING = os.getenv("CONNECTION_STRING") # the connection string for our MongoDB cluster (which has the DB)
     client = MongoClient(CONNECTION_STRING) # 'client': the MongoDB client that's now connected to our MongoDB cluster
     return client['recipeDatabase'] # returns our 'recipeDatabase' DB from the cluster
+
+def get_recipes_table():
+    # returns our 'recipes' table
+    recipeDatabase = get_DB() # our app database
+    return recipeDatabase['recipes'] # the 'recipes' collection in our database
